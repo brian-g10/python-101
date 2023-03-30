c5ce6ebb9536de7517d08cdb72341eaf3b50b7c6
brian-g10 <129234515+brian-g10@users.noreply.github.com>
2023-03-30 12:06:37 -0700

    correct program
    otherwise, it doubles ".. bottles of beer on the wall"

--- 4-loops/20_99_bottles.py
+++ 4-loops/20_99_bottles.py
@@ -1,8 +1,8 @@
 # 99 Bottles of Beer 🍻
 # Codédex
 
+print('99 bottles of beer on the wall')
 for i in range(99, 0, -1):
-  print(f'{i} bottles of beer on the wall')
-  print(f'{i} bottles of beer')
-  print('take one down pass it around')
-  print(f'{i-1} bottles of beer on the wall')
+    print(f'{i} bottles of beer')
+    print('take one down, pass it around')
+    print(f'{i-1} bottles of beer on the wall')
